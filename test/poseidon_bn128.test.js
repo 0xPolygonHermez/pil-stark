957907c87e8fa117f141f584bfbc527ac750c3d4
eigmax <stephen@ieigen.com>
2022-10-31 13:45:05 +0800

    fix poseidon_bn128 UT

--- test/poseidon_bn128.test.js
+++ test/poseidon_bn128.test.js
@@ -2,8 +2,7 @@ const chai = require("chai");
 const assert = chai.assert;
 const { getCurveFromName, Scalar, F1Field } = require("ffjavascript");
 
-const buildPoseidon = require("../src/poseidon");
-const buildPoseidonRef = require("circomlibjs").buildPoseidon;
+const { buildPoseidonReference, buildPoseidon, buildPoseidonWasm } = require("circomlibjs");
 
 describe("Poseidon test", async function () {
     let poseidon;
@@ -13,14 +12,14 @@ describe("Poseidon test", async function () {
 
     before(async () => {
         poseidon = await buildPoseidon();
-        poseidonRef = await buildPoseidonRef();
+        poseidonRef = await buildPoseidonReference();
         const bn128 = await getCurveFromName("bn128", true, buildPoseidonWasm);
         F = bn128.Fr;
     });
 
     it("Should check constrain reference implementation poseidonperm_x5_254_3", async () => {
         const input = [];
-        for (let i=0; i<16; i++) input[i] = F.e(i);
+        for (let i = 0; i < 16; i++) input[i] = F.e(i);
         const resRef = poseidonRef(input);
         const res = poseidon(input);
 
