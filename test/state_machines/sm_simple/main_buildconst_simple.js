d5f24b995a95941f2040be75b99178b69e333322
RogerTaule <roger.taulee@gmail.com>
2023-07-02 20:28:08 +0200

    Adding tasks for simple

--- test/state_machines/sm_simple/main_buildconst_simple.js
+++ test/state_machines/sm_simple/main_buildconst_simple.js
@@ -5,6 +5,7 @@ const { newConstantPolsArray, compile } = require("pilcom");
 const smSimple = require("./sm_simple.js");
 
 const F3g = require("../../../src/helpers/f3g");
+const { F1Field } = require("ffjavascript");
 
 
 const argv = require("yargs")
@@ -17,14 +18,22 @@ async function run() {
 
     const outputFile = typeof(argv.output) === "string" ?  argv.output.trim() : "simple.const";
 
-    const F = new F3g();
-    const pil = await compile(F, path.join(__dirname, "simple1.pil"));
+    if(argv.curve && !["gl", "bn128"].includes(argv.curve)) throw new Error("Curve not supported");
+    
+    const curveName = argv.curve || "gl";
+    const F = curveName === "gl" ? new F3g : new F1Field(21888242871839275222246405745257275088548364400416034343698204186575808495617n);
+
+    const pil = await compile(F, path.join(__dirname, "simple2.pil"));
 
     const constPols = newConstantPolsArray(pil, F);
 
     await smSimple.buildConstants(constPols.Simple);
 
-    await constPols.saveToFile(outputFile);
+    if(curveName === "gl"){
+        await constPols.saveToFile(outputFile);
+    } else {
+        await constPols.saveToFileFr(outputFile);
+    }
 
     console.log("file Generated Correctly");
 }
--- test/state_machines/sm_simple/main_exec_simple.js
+++ test/state_machines/sm_simple/main_exec_simple.js
@@ -5,6 +5,7 @@ const version = require("../../../package").version;
 const smSimple = require("./sm_simple.js");
 
 const F3g = require("../../../src/helpers/f3g");
+const { F1Field } = require("ffjavascript");
 const { newCommitPolsArray, compile } = require("pilcom");
 
 
@@ -18,14 +19,22 @@ async function run() {
 
     const outputFile = typeof(argv.output) === "string" ?  argv.output.trim() : "simple.commit";
 
-    const F = new F3g();
-    const pil = await compile(F, path.join(__dirname, "simple1.pil"));
+    if(argv.curve && !["gl", "bn128"].includes(argv.curve)) throw new Error("Curve not supported");
+    
+    const curveName = argv.curve || "gl";
+    const F = curveName === "gl" ? new F3g : new F1Field(21888242871839275222246405745257275088548364400416034343698204186575808495617n);
+
+    const pil = await compile(F, path.join(__dirname, "simple2.pil"));
 
     const cmPols =  newCommitPolsArray(pil, F);
 
-    await smSimple.execute(cmPols.Simple);
+    await smSimple.execute(F, cmPols.Simple);
 
-    await cmPols.saveToFile(outputFile);
+    if(curveName === "gl"){
+        await cmPols.saveToFile(outputFile);
+    } else {
+        await cmPols.saveToFileFr(outputFile);
+    }
 
     console.log("file Generated Correctly");
 }
