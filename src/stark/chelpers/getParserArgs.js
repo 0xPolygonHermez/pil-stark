30d187997d587460d32709a2a1df973bd36b3783
RogerTaule <roger.taulee@gmail.com>
2024-01-29 16:56:49 +0100

    Splitting functions and refactor

---
+++ src/stark/chelpers/getParserArgs.js
@@ -0,0 +1,229 @@
+const { assert } = require("chai");
+const { getIdMaps } = require("./helpers");
+const { getOperation } = require("./generateParser");
+
+const operationsTypeMap = {
+    "add": 0,
+    "sub": 1,
+    "mul": 2,
+    "copy": 3,
+}
+
+module.exports.getParserArgs = function getParserArgs(starkInfo, operations, code, dom, stage, executeBefore) {
+
+    var ops = [];
+    var cont_ops = 0;
+    var opsString = "{"
+    var args = [];
+    var cont_args = 0;
+    var argsString = "{"
+
+    var counters_ops = [];
+
+    const nBits = starkInfo.starkStruct.nBits;
+    const nBitsExt = starkInfo.starkStruct.nBitsExt;
+
+    const next = (dom == "n" ? 1 : 1 << (nBitsExt - nBits)).toString();
+    const N = (dom == "n" ? (1 << nBits) : (1 << nBitsExt)).toString();
+
+    // Evaluate max and min temporal variable for tmp_ and tmp3_
+    let maxid = 100000;
+    let ID1D = new Array(maxid).fill(-1);
+    let ID3D = new Array(maxid).fill(-1);
+    let { count1d, count3d } = getIdMaps(maxid, ID1D, ID3D, code);
+        
+    for (let j = 0; j < code.length; j++) {
+        const r = code[j];
+        args.push(operationsTypeMap[r.op]);
+        argsString += `${operationsTypeMap[r.op]}, `;
+        ++cont_args;
+        pushResArg(r, r.dest.type);
+        ++cont_ops;
+        
+        for(let i = 0; i < r.src.length; i++) {
+            pushSrcArg(r.src[i], r.src[i].type);
+        }
+
+        let operation = getOperation(r);
+        let opsIndex = operations.findIndex(op => 
+            op.dest_type === operation.dest_type
+            && op.src0_type === operation.src0_type
+            && ((!op.hasOwnProperty("src1_type")) || (op.src1_type === operation.src1_type)));
+        
+            
+        if (opsIndex === -1) throw new Error("Operation not considered: " + JSON.stringify(operation));
+
+        ops.push(opsIndex);
+        opsString += `${opsIndex}, `;
+
+        if(!counters_ops[opsIndex]) counters_ops[opsIndex] = 0;
+        counters_ops[opsIndex] += 1;
+    }
+
+    assert(cont_ops == ops.length);
+    assert(cont_args == args.length);
+
+    console.log("Number of operations: ", cont_ops, ops.length);
+    console.log("Number of arguments: ", cont_args, args.length);
+    console.log("Different operations types: ", operations.length);
+    console.log("--------------------------------");
+
+    if(opsString !== "{") opsString = opsString.substring(0, opsString.lastIndexOf(","));
+    opsString += "};"
+    if(argsString !== "{") argsString = argsString.substring(0, argsString.lastIndexOf(","));
+    argsString += "};"
+
+    const stageInfo = {
+        stage,
+        executeBefore: executeBefore ? 1 : 0,
+        domainSize: dom === "n" ? (1 << nBits) : (1 << nBitsExt),
+        nTemp1: count1d,
+        nTemp3: count3d,
+        nOps: cont_ops,
+        ops,
+        nArgs: cont_args,
+        args: args.map(v => typeof v === 'string' && v.endsWith('ULL') ? parseInt(v.replace('ULL', '')) : v),
+    }
+    
+    return stageInfo;
+
+    function pushResArg(r, type) {
+        let eDst;
+        switch (type) {
+            case "tmp": {
+                if (r.dest.dim == 1) {
+                    args.push(ID1D[r.dest.id]);
+                    argsString += `${ID1D[r.dest.id]}, `;
+                } else {
+                    assert(r.dest.dim == 3);
+                    args.push(ID3D[r.dest.id]);
+                    argsString += `${ID3D[r.dest.id]}, `;
+                }
+                cont_args += 1;
+                break;
+            }
+            case "q": {
+                break;
+            }
+            case "cm": {
+                if (dom == "n") {
+                    eDst = evalMap_(starkInfo.cm_n[r.dest.id], r.dest.prime)
+                } else if (dom == "2ns") {
+                    eDst = evalMap_(starkInfo.cm_2ns[r.dest.id], r.dest.prime)
+                } else {
+                    throw new Error("Invalid dom");
+                }
+                break;
+            }
+            case "tmpExp": {
+                if (dom == "n") {
+                    eDst = evalMap_(starkInfo.tmpExp_n[r.dest.id], r.dest.prime)
+                } else if (dom == "2ns") {
+                    throw new Error("Invalid dom");
+                } else {
+                    throw new Error("Invalid dom");
+                }
+                break;
+            }
+            case "f": {
+                break;
+            }
+            default: throw new Error("Invalid reference type set: " + r.dest.type);
+        }
+        return eDst;
+    }
+
+
+    function pushSrcArg(r, type) {
+        switch (type) {
+            case "tmp": {
+                if (r.dim == 1) {
+                    args.push(ID1D[r.id]);
+                    argsString += `${ID1D[r.id]}, `;
+                } else {
+                    assert(r.dim == 3);
+                    args.push(ID3D[r.id]);
+                    argsString += `${ID3D[r.id]}, `;
+
+                }
+                cont_args += 1;
+                break;
+            }
+            case "const": {
+                let offset_prime = r.prime ? next : 0;
+
+                args.push(r.id);
+                args.push(offset_prime);
+
+                argsString += `${r.id}, `;
+                argsString += `${offset_prime}, `;
+                
+                cont_args += 2;
+                break;
+            }
+            case "tmpExp": {
+                if (dom == "n") {
+                    evalMap_(starkInfo.tmpExp_n[r.id], r.prime)
+                } else if (dom == "2ns") {
+                    throw new Error("Invalid dom");
+                } else {
+                    throw new Error("Invalid dom");
+                }
+                break;
+            }
+            case "cm": {
+                if (dom == "n") {
+                    evalMap_(starkInfo.cm_n[r.id], r.prime)
+                } else if (dom == "2ns") {
+                    evalMap_(starkInfo.cm_2ns[r.id], r.prime)
+                } else {
+                    throw new Error("Invalid dom");
+                }
+                break;
+            }
+            case "q": {
+                if (dom == "n") {
+                    throw new Error("Accessing q in domain n");
+                } else if (dom == "2ns") {
+                    evalMap_(starkInfo.qs[r.id], r.prime)
+                } else {
+                    throw new Error("Invalid dom");
+                }
+                break;
+            }
+            case "number": {
+                args.push(`${BigInt(r.value).toString()}ULL`);
+                argsString += `${BigInt(r.value).toString()}ULL, `;
+                cont_args += 1;
+                break;
+            }
+            case "public":
+            case "challenge":
+            case "eval": 
+            {
+                args.push(r.id);
+                argsString += `${r.id}, `;
+                cont_args += 1;
+                break;
+            }
+        }
+    }
+
+    function evalMap_(polId, prime) {
+        let p = starkInfo.varPolMap[polId];
+
+        let offset = starkInfo.mapOffsets[p.section] + p.sectionPos;
+        let offset_prime = prime ? next : 0;
+        let size = starkInfo.mapSectionsN[p.section];
+
+        args.push(Number(offset));
+        args.push(Number(offset_prime));
+        args.push(Number(size));
+
+        argsString += `${offset}, `;
+        argsString += `${offset_prime}, `;
+        argsString += `${N}, `;
+        
+        cont_args += 3;
+    }
+}
--- src/stark/chelpers/stark_chelpers.js
+++ src/stark/chelpers/stark_chelpers.js
@@ -1,8 +1,12 @@
-const { writeCHelpersFile } = require("./binFile.js");
-const compileCode_parser = require("./compileCode_parser.js");
+const { getParserArgs } = require("./getParserArgs.js");
+const { generateParser } = require("./generateParser.js");
 const { compileCode } = require("./helpers.js");
 
 module.exports = async function buildCHelpers(starkInfo, config = {}) {
+
+    const isGeneric = config && config.isGeneric || false;
+
+    let result = {};
 
     const code = [];
     const multipleCodeFiles = config && config.multipleCodeFiles;
@@ -10,39 +14,22 @@ module.exports = async function buildCHelpers(starkInfo, config = {}) {
 
     const cHelpersInfo = [];
 
-    for (let i = 0; i < starkInfo.nPublics; i++) {
-        if (starkInfo.publicsCode[i]) {
-            code.push(compileCode(`${config.className}::publics_" + i`, starkInfo, starkInfo.publicsCode[i].first, "n", true));
-        }
-    }
+    let operations;
 
-    const pubTable = [];
-    pubTable.push("publics = (")
-    for (let i = 0; i < starkInfo.nPublics; i++) {
-        const comma = i == 0 ? "     " : "     ,";
-        if (starkInfo.publicsCode[i]) {
-            pubTable.push(`${comma}(publics_${i}_first, publics_${i}_i,  publics_${i}_last)`);
-        } else {
-            pubTable.push(`${comma}(NULL,NULL,NULL)`);
-        }
-    }
-    pubTable.push(");");
-
-    let result = {};
-
-    if (multipleCodeFiles) {
-        result.public = pubTable.join("\n") + "\n";
-    }
-    else {
-        code.push(pubTable.join("\n"));
+    if(isGeneric) {
+        const parser = generateParser();
+        operations = parser.operations;
+        result.generic_parser_cpp = parser.parserCPPCode;
     }
 
     if (optcodes && multipleCodeFiles) {
-        const step2PrevParser = compileCode_parser(starkInfo, config, starkInfo.step2prev.first, "n", 2, true);
-        cHelpersInfo.push(step2PrevParser.stageInfo);
-        if(step2PrevParser.parserCPPCode) {
-            result.step2prev_parser_cpp = step2PrevParser.parserCPPCode;
+        if(!isGeneric) {
+            const parser = generateParser(starkInfo.step2prev.first);
+            operations = parser.operations;
+            result.step2prev_parser_cpp = parser.parserCPPCode;
         }
+        const step2StageInfo = getParserArgs(starkInfo, operations, starkInfo.step2prev.first, "n", 2, true);
+        cHelpersInfo.push(step2StageInfo);
     }
 
     code.push(compileCode(`${config.className}::step2prev`, starkInfo, starkInfo.step2prev.first, "n"));
@@ -53,11 +40,14 @@ module.exports = async function buildCHelpers(starkInfo, config = {}) {
     }
 
     if (optcodes && multipleCodeFiles) {
-        const step3PrevParser = compileCode_parser(starkInfo, config, starkInfo.step3prev.first, "n", 3, true)
-        cHelpersInfo.push(step3PrevParser.stageInfo);
-        if(step3PrevParser.parserCPPCode) {
-            result.step3prev_parser_cpp = step3PrevParser.parserCPPCode;
+        if(!isGeneric) {
+            const parser = generateParser(starkInfo.step3prev.first);
+            operations = parser.operations;
+            result.step3prev_parser_cpp = parser.parserCPPCode;
         }
+        const step3PrevStageInfo = getParserArgs(starkInfo, operations, starkInfo.step3prev.first, "n", 3, true)
+        cHelpersInfo.push(step3PrevStageInfo);
+
     }
 
     code.push(compileCode(`${config.className}::step3prev`, starkInfo, starkInfo.step3prev.first, "n"));
@@ -68,11 +58,13 @@ module.exports = async function buildCHelpers(starkInfo, config = {}) {
     }
 
     if (optcodes && multipleCodeFiles) {
-        const step3Parser = compileCode_parser(starkInfo, config, starkInfo.step3.first, "n", 3, false);
-        cHelpersInfo.push(step3Parser.stageInfo);
-        if(step3Parser.parserCPPCode) {
-            result.step3_parser_cpp = step3Parser.parserCPPCode;
+        if(!isGeneric) {
+            const parser = generateParser(starkInfo.step3.first);
+            operations = parser.operations;
+            result.step3_parser_cpp = parser.parserCPPCode;
         }
+        const step3StageInfo = getParserArgs(starkInfo, operations, starkInfo.step3.first, "n", 3, false);
+        cHelpersInfo.push(step3StageInfo);
     }
 
     code.push(compileCode(`${config.className}::step3`, starkInfo, starkInfo.step3.first, "n"));
@@ -83,11 +75,13 @@ module.exports = async function buildCHelpers(starkInfo, config = {}) {
     }
 
     if (optcodes && multipleCodeFiles) {
-        const step42nsParser = compileCode_parser(starkInfo, config, starkInfo.step42ns.first, "2ns", 4, true);
-        cHelpersInfo.push(step42nsParser.stageInfo);
-        if(step42nsParser.parserCPPCode) {
-            result.step42ns_parser_cpp = step42nsParser.parserCPPCode;
+        if(!isGeneric) {
+            const parser = generateParser(starkInfo.step42ns.first);
+            operations = parser.operations;
+            result.step42ns_parser_cpp = parser.parserCPPCode;
         }
+        const step42nsStageInfo = getParserArgs(starkInfo, operations, starkInfo.step42ns.first, "2ns", 4, true);
+        cHelpersInfo.push(step42nsStageInfo);
     }
     code.push(compileCode(`${config.className}::step42ns`, starkInfo, starkInfo.step42ns.first, "2ns"));
 
@@ -97,21 +91,21 @@ module.exports = async function buildCHelpers(starkInfo, config = {}) {
     }
 
     if (optcodes && multipleCodeFiles) {
-        const step52nsParser = compileCode_parser(starkInfo, config, starkInfo.step52ns.first, "2ns", 5, true);
-        cHelpersInfo.push(step52nsParser.stageInfo);
-        if(step52nsParser.parserCPPCode) {
-            result.step52ns_parser_cpp = step52nsParser.parserCPPCode;
+        if(!isGeneric) {
+            const parser = generateParser(starkInfo.step52ns.first);
+            operations = parser.operations;
+            result.step52ns_parser_cpp = parser.parserCPPCode;
         }
+        const step52nsStageInfo = getParserArgs(starkInfo, operations, starkInfo.step52ns.first, "2ns", 5, true);
+        cHelpersInfo.push(step52nsStageInfo);
     }
-
-    await writeCHelpersFile(config.binFile, cHelpersInfo);
 
     code.push(compileCode(`${config.className}::step52ns`, starkInfo, starkInfo.step52ns.first, "2ns"));
 
     if (multipleCodeFiles) {
         result.step52ns = code.join("\n\n") + "\n";
-        return result;
+        return {code: result, cHelpersInfo }
     }
 
-    return code.join("\n\n");
+    return {code: code.join("\n\n"), cHelpersInfo };
 }
